f6e3191415a78340c1483a552090f39a69e564a5
staticGuru <vigneshguru274@gmail.com>
2024-01-13 12:08:25 +0530

    create the chunks from text

--- HF_Langchain.py
+++ HF_Langchain.py
@@ -1,16 +1,28 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 
 def get_pdf_content(pdf_docs):
     text=""
     for pdf in pdf_docs:
         pdf_reader= PdfReader(pdf)
-        for page in Pdf_reader.pages():
-            text=text+page.extractText()
+        for page in pdf_reader.pages:
+            text=text+page.extract_text()
     return text
 
+def get_text_chunks(raw_text):
+    text_splitter=CharacterTextSplitter(
+      separator='\n',
+      chunk_overlap=200,
+      chunk_size=1000,
+      length_function=len  
+    )
+    chunks=text_splitter.split_text(raw_text)
+    return chunks
+    
+    
 
 def PDFbot():
     load_dotenv()
@@ -22,12 +34,12 @@ def PDFbot():
         pdf_docs= st.file_uploader('Upload your documents(PDF) here & tab the "Process"',accept_multiple_files=True)
         if st.button('Process'):
             with st.spinner('Processing...'):
-                raw_text=get_pdf_content(pdf_docs)
                 #get pdf text
-                
+                raw_text=get_pdf_content(pdf_docs)
                 
                 #get the text chuncks
-                
+                text_chuncks=get_text_chunks(raw_text)
+                st.write(text_chuncks)
                 
                 #create the vector store 
 
