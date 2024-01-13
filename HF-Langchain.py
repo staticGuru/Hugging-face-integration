77642cae4f5f88a6326073f07e6ce41dc037bba7
staticGuru <vigneshguru274@gmail.com>
2024-01-13 09:34:14 +0530

    Add the streamlit UI data

--- HF-Langchain.py
+++ HF-Langchain.py
@@ -3,6 +3,12 @@ from streamlit import st
 
 def main():
     st.title('I will answer your question from your\'s PDF')
-
+    st.text_input('Ask me anythings!!!:)')
+    
+    with st.sidebar:
+        st.subHeader('Your\'s Datasource(PDF)')
+        st.file_uploader('Upload your documents(PDF) here & tab the "Process"')
+        st.button('Process')
+        
 if __name__ == '__main__':
     main()
