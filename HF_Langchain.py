8f7fdbe3401942573b04befec6d9945958967364
staticGuru <vigneshguru274@gmail.com>
2024-01-13 11:42:38 +0530

    Read the PDF documents

--- HF_Langchain.py
+++ HF_Langchain.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_content(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in Pdf_reader.pages():
+            text=text+page.extractText()
+    return text
+
 
 def PDFbot():
     load_dotenv()
@@ -9,5 +20,14 @@ def PDFbot():
     with st.sidebar:
         st.subheader('Your\'s Datasource(PDF)')
         pdf_docs= st.file_uploader('Upload your documents(PDF) here & tab the "Process"',accept_multiple_files=True)
-        st.button('Process')
+        if st.button('Process'):
+            with st.spinner('Processing...'):
+                raw_text=get_pdf_content(pdf_docs)
+                #get pdf text
+                
+                
+                #get the text chuncks
+                
+                
+                #create the vector store 
 
